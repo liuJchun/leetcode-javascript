99f20d24b8e222907a7fde3047064aeafa1e589b
jianchun.liu <jianchun.liu@wayz.ai>
2022-06-08 10:18:59 +0800

    链表

---
+++ 143.重排链表.js
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=143 lang=javascript
+ *
+ * [143] 重排链表
+ *
+ * https://leetcode-cn.com/problems/reorder-list/description/
+ *
+ * algorithms
+ * Medium (63.45%)
+ * Likes:    869
+ * Dislikes: 0
+ * Total Accepted:    169.4K
+ * Total Submissions: 266.9K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个单链表 L 的头节点 head ，单链表 L 表示为：
+ *
+ *
+ * L0 → L1 → … → Ln - 1 → Ln
+ *
+ *
+ * 请将其重新排列后变为：
+ *
+ *
+ * L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+ *
+ * 不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[1,4,2,3]
+ *
+ * 示例 2：
+ *
+ *
+ *
+ *
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[1,5,2,4,3]
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表的长度范围为 [1, 5 * 10^4]
+ * 1 <= node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function (head) {
+    if (!head) return null
+
+    const formatList = reverseList(head)
+
+    console.log(formatList, head)
+}
+
+function reverseList(head) {
+    let pre = null,
+        cur = head
+    while (cur) {
+        // const next = cur.next
+        // cur.next = pre
+        // pre = cur
+        // cur = next
+        ;[cur.next, pre, cur] = [pre, cur, cur.next]
+    }
+    return pre
+}
+// @lc code=end
